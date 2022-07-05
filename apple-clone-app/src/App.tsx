a3c665c292f94a5bf85d2494e9431c816557206f
arunlalpp <arunlalpp@gmail.com>
2022-07-06 01:13:29 +0530

    Feat:"Header for mobile"

--- apple-clone-app/src/App.tsx
+++ apple-clone-app/src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div>
-      <h1 className="text-red-500 text-2xl font-extrabold">Welcome</h1>
+      <Header />
     </div>
   );
 }
