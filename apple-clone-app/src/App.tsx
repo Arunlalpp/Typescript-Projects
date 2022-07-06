df74206291a5cf93af467a1bbda12cbc96537b49
arunlalpp <arunlalpp@gmail.com>
2022-07-06 20:06:54 +0530

    feat:"banner image prop and text"

--- apple-clone-app/src/App.tsx
+++ apple-clone-app/src/App.tsx
@@ -1,12 +1,27 @@
 import './App.css';
 import MobileHeader from "./components/MobileHeader";
 import Navlinks from "./components/Navlinks";
+import Bannersection from "./components/Bannersection";
+import bannerImage from "./components/Images/Bannerimage.jpg";
+import Button from "./components/Shared/Button";
 
 function App() {
 	return (
 		<div>
 			<MobileHeader />
 			<Navlinks />
+			<div className="bg-[#fbfbfd] py-3 text-center">
+				<span className="text-sm flex flex-row justify-center items-center">
+					Get 5% instant Cashback up to ₹6000 with qualifying HDFC Bank credit
+					cards on orders over ₹54900.
+					<Button ButtonText="Learn more" />
+				</span>
+			</div>
+			<Bannersection
+				url={bannerImage}
+				title="iphone 13"
+				SubText="Your new superpower"
+			/>
 		</div>
 	);
 }
