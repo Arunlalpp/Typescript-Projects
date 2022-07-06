2017cc7d30c55bfe0273a94ffb8a229187e07c0b
arunlalpp <arunlalpp@gmail.com>
2022-07-07 00:59:46 +0530

    Feat:"Add card image container "

---
+++ apple-clone-app/src/components/CardContainer.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Button from "./Shared/Button";
+type MankindProps = {
+	url: string;
+};
+
+function CardContainer({ url }: MankindProps) {
+	return (
+		<div className="">
+			<div className="relative">
+				<img src={url} alt="" />
+
+				<div className="absolute top-[20%] left-[40%] text-center ">
+					<h1>MacBoook Air</h1>
+					<span>
+						Supercharged By
+						<img src="" alt="" />
+					</span>
+					<p>kjgfrivweriwhefkjwbdigrebb vciwuedgfubksdc ng</p>
+					<div className="flex flex-row gap-4 justify-center">
+						<Button ButtonText="Learn more" />
+						<Button ButtonText="View pricing" />
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default CardContainer;
