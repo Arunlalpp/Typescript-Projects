972f9efe7af0c83ab1e4721b017b2803cd2eabce
arunlalpp <arunlalpp@gmail.com>
2022-07-08 18:58:47 +0530

    Feat:"Define component"

--- neurowzer-app/src/App.tsx
+++ neurowzer-app/src/App.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import "./App.css";
+import LearningJourney from "./components/LearningJourney";
+import SelfHelp from "./components/SelfHelp";
+import bannerImage from "./Images/BannerImage.jpg";
 
 function App() {
 	return (
-		<div className="App">
-			<h1 className="text-red-700">welcome</h1>
+		<div>
+			{/* <LearningJourney
+				CardTitle="6 Pillers of Brain Health"
+				SubTitle="“Even the highly successful people have only 24 hours to
+			work.”"
+				Subtext="YEAT SMART, THINK BETTER. As you grow older, your brain is
+			affected by lifestyle and environmental factors, resulting in
+			a process called oxidation, which damages brain cells."
+			/> */}
+			<SelfHelp url={bannerImage} />
 		</div>
 	);
 }
