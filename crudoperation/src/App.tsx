67ae5eb98b595b9532ef991046360bca362dbcfd
Arunlal PP <arunlalpp@gmail.com>
2023-08-03 00:10:43 +0530

    feat: call user API

--- crudoperation/src/App.tsx
+++ crudoperation/src/App.tsx
@@ -17,15 +17,47 @@ function App() {
 
   // Network request
   useEffect(() => {
+    setState({ ...state, loading: true });
     UserService.getAllUsers()
-      .then((res) => console.log(res.data))
-      .catch();
+      .then((res) => setState({ ...state, loading: false, users: res.data }))
+      .catch((err) =>
+        setState({ ...state, loading: false, errorMessage: err.message })
+      );
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  const { loading, users, errorMessage } = state;
 
   return (
     <div className="">
-      Welcome
-      <div></div>
+      <div>
+        {errorMessage && <p>{errorMessage}</p>}
+        {loading && <p>LOading....</p>}
+        <table>
+          <thead>
+            <tr>
+              <td>ID</td>
+              <td>Name</td>
+              <td>User Name</td>
+              <td>Email</td>
+              <td>Phone</td>
+            </tr>
+          </thead>
+          <tbody>
+            {users.length > 0 &&
+              users.map((user) => (
+                <tr key={user.id}>
+                  <td>{user.id}</td>
+                  <td>{user.name}</td>
+                  <td>{user.username}</td>
+                  <td>{user.email}</td>
+                  <td>{user.phone}</td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
