651142975b4fd4c8fc3a7b612238a5a3995bdfd7
Arunlal PP <arunlalpp@gmail.com>
2023-08-03 11:16:15 +0530

    feat: style table

--- crudoperation/src/App.tsx
+++ crudoperation/src/App.tsx
@@ -1,63 +1,10 @@
-import React, { useEffect, useState } from "react";
-import { IUser } from "./types";
-import { UserService } from "./endpoint/UserService";
-
-export interface IState {
-  loading: boolean;
-  users: IUser[];
-  errorMessage: string;
-}
+import React from "react";
+import UserDetails from "./components/UserDetails";
 
 function App() {
-  const [state, setState] = useState<IState>({
-    loading: false,
-    users: [] as IUser[],
-    errorMessage: "",
-  });
-
-  // Network request
-  useEffect(() => {
-    setState({ ...state, loading: true });
-    UserService.getAllUsers()
-      .then((res) => setState({ ...state, loading: false, users: res.data }))
-      .catch((err) =>
-        setState({ ...state, loading: false, errorMessage: err.message })
-      );
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  const { loading, users, errorMessage } = state;
-
   return (
-    <div className="">
-      <div>
-        {errorMessage && <p>{errorMessage}</p>}
-        {loading && <p>LOading....</p>}
-        <table>
-          <thead>
-            <tr>
-              <td>ID</td>
-              <td>Name</td>
-              <td>User Name</td>
-              <td>Email</td>
-              <td>Phone</td>
-            </tr>
-          </thead>
-          <tbody>
-            {users.length > 0 &&
-              users.map((user) => (
-                <tr key={user.id}>
-                  <td>{user.id}</td>
-                  <td>{user.name}</td>
-                  <td>{user.username}</td>
-                  <td>{user.email}</td>
-                  <td>{user.phone}</td>
-                </tr>
-              ))}
-          </tbody>
-        </table>
-      </div>
+    <div className="flex justify-center items-center bg-slate-700">
+      <UserDetails />
     </div>
   );
 }
---
+++ crudoperation/src/components/UserDetails.tsx
@@ -0,0 +1,80 @@
+import React, { useEffect, useState } from "react";
+import { IUser } from "../types";
+import { UserService } from "../endpoint/UserService";
+
+export interface IState {
+  loading: boolean;
+  users: IUser[];
+  errorMessage: string;
+}
+
+function UserDetails() {
+  const [state, setState] = useState<IState>({
+    loading: false,
+    users: [] as IUser[],
+    errorMessage: "",
+  });
+
+  // Network request
+  useEffect(() => {
+    setState({ ...state, loading: true });
+    UserService.getAllUsers()
+      .then((res) => setState({ ...state, loading: false, users: res.data }))
+      .catch((err) =>
+        setState({ ...state, loading: false, errorMessage: err.message })
+      );
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const { loading, users, errorMessage } = state;
+
+  const tableHeadData = ["ID", "Name", "User Name", "Email", "Phone"];
+  return (
+    <div className="p-8">
+      {errorMessage && <p>{errorMessage}</p>}
+      {loading && <p>LOading....</p>}
+      <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead className="text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+          <tr className="bg-white border-b dark:bg-green-500 dark:border-gray-700">
+            {tableHeadData.map((item) => (
+              <td
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-black"
+                key={item}
+              >
+                {item}
+              </td>
+            ))}
+          </tr>
+        </thead>
+        <tbody className="bg-blue-300 py-2">
+          {users.length > 0 &&
+            users.map((user) => (
+              <tr
+                key={user.id}
+                className="bg-white border-b dark:bg-gray-900 dark:border-gray-700"
+              >
+                <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                  {user.id}
+                </td>
+                <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                  {user.name}
+                </td>
+                <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                  {user.username}
+                </td>
+                <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                  {user.email}
+                </td>
+                <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                  {user.phone}
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default UserDetails;
