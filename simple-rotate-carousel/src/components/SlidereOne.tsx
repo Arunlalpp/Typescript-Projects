b3f50e0a43f2e8678d068f0f594b4bfe6044658a
Arunlal PP <75899600+Arunlalpp@users.noreply.github.com>
2023-08-30 22:00:17 +0530

    feat: add style to the component (#11)

--- simple-rotate-carousel/src/components/SlidereOne.tsx
+++ simple-rotate-carousel/src/components/SlidereOne.tsx
@@ -1,7 +1,30 @@
 import React from "react";
 
-function SliderOne() {
-  return <div></div>;
+interface SliderOneProps {
+  bgURL: string;
+}
+
+function SliderOne({ bgURL }: SliderOneProps) {
+  return (
+    <div className="h-full w-full">
+      <div
+        className="h-full w-full flex flex-col justify-center items-center"
+        style={{
+          backgroundImage: `url(${bgURL})`,
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "bottom",
+          backgroundSize: "cover",
+        }}
+      >
+        <p className="text-white text-left font-medium text-xs">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum
+          aliquid quas distinctio quam assumenda quibusdam, odit voluptas. Sit
+          obcaecati laudantium voluptate earum libero quidem quibusdam facilis.
+          Ut ipsum dolores ullam.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default SliderOne;
