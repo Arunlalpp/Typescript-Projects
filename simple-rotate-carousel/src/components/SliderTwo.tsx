12f2de7caae116cc303555177566b9f00d0f450d
Arunlal PP <arunlalpp@gmail.com>
2023-09-02 11:21:50 +0530

    feat: react simple slider carousel with components render

--- simple-rotate-carousel/src/components/SliderTwo.tsx
+++ simple-rotate-carousel/src/components/SliderTwo.tsx
@@ -1,36 +1,79 @@
 import React from "react";
+import SliderOne from "./SliderOne";
 
-export interface SliderTwoProps {
-  bgURL: string;
-  header: string;
-  collections?: string[];
+export enum ScreenTypes {
+  Initial = "initial",
+  Second = "second",
+  Final = "final",
 }
 
-function SliderTwo({ bgURL, header, collections }: SliderTwoProps) {
-  return (
-    <div className="h-full w-full">
-      <div
-        className="w-full h-full"
-        style={{
-          backgroundImage: `url(${bgURL})`,
-          backgroundPosition: "bottom",
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "cover",
-        }}
-      >
-        <div className="flex flex-col justify-center items-center pt-8">
-          <p className="text-white font-bold text-center text-xl">{header}</p>
+interface Props {
+  title: string;
+  screenType: ScreenTypes;
+  bgImg: string;
+}
+
+function SliderTwo({ title, screenType, bgImg }: Props) {
+  const render = () => {
+    if (screenType === ScreenTypes.Initial) {
+      return (
+        <div className="p-5 text-center">
+          SampleText
+          <br />
+          <br />
+          SampleText@
         </div>
-        <div className="h-full w-full flex justify-center items-center">
-          <div className="bg-white px-6 h-3/4 w-3/4">
-            <ul className="py-4">
-              {collections?.map((items) => (
-                <li className="list-disc">{items}</li>
-              ))}
-            </ul>
+      );
+    }
+    if (screenType === ScreenTypes.Final) {
+      return (
+        <div className="p-5 text-center">
+          <p className="text-body-large font-medium text-nw-grey-type px-4">
+            Dummy
+          </p>
+          <div className="flex flex-col items-center justify-center pb-3 pt-7 px-6">
+            <img
+              className="pb-3"
+              src="https://images.pexels.com/photos/777059/pexels-photo-777059.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt="internet"
+            />
+            <p className="font-bold text-nw-grey-type">DummyOne</p>
+          </div>
+          <div className="flex flex-col items-center justify-center pb-3 pt-7 px-6">
+            <img
+              className="pb-3"
+              src="https://images.pexels.com/photos/632522/pexels-photo-632522.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt="DND"
+            />
+            <p className="font-bold text-nw-grey-type">2.DummyTwo</p>
           </div>
         </div>
+      );
+    }
+    return (
+      <div className="w-full h-full pb-32">
+        <SliderOne />
       </div>
+    );
+  };
+
+  return (
+    <div
+      style={{ backgroundImage: `url(${bgImg})`, backgroundSize: "cover" }}
+      className="w-full h-full flex flex-col whitespace-normal px-6"
+    >
+      <div className="w-full pt-10 flex items-center justify-end">
+        <button
+          className="py-4 font-semibold text-callout-02 text-nw-grey-type"
+          type="button"
+        >
+          Skip
+        </button>
+      </div>
+      <div className="w-full px-14 pb-5">
+        <p className="text-heading-04 font-semibold text-center">{title}</p>
+      </div>
+      {render()}
     </div>
   );
 }
