79eb370e592b6bcfea7d56cb58ebdd2bc8eb3c5e
Arunlal PP <arunlalpp@gmail.com>
2023-09-20 10:24:30 +0530

    refactor: remove unusede codes

--- student/src/components/StudentsActions.tsx
+++ student/src/components/StudentsActions.tsx
@@ -33,8 +33,6 @@ export default function StudentsDetails() {
 
   const handleAddStudent = () => {
     setStudentsInfo([...studentsInfo, currentStudent]);
-    console.log({ currentStudent });
-
     setCurrentStudent(initialStudentsInfo);
   };
 
