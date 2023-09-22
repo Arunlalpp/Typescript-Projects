1b75d7b951b2f9970ab59f49bade93de374c014f
Arunlal PP <75899600+Arunlalpp@users.noreply.github.com>
2023-09-22 10:55:54 +0530

    feat: add styles (#23)

--- search-filter/src/App.js
+++ search-filter/src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const productList = [
+    "blue pant",
+    "black pant",
+    "blue shirt",
+    "black shoes",
+    "brown shoes",
+    "white pant",
+    "white shoes",
+    "red shirt",
+    "gray pant",
+    "white shirt",
+    "golden shoes",
+    "dark pant",
+    "pink shirt",
+    "yellow pant",
+  ];
+
+  const [products, setProducts] = useState(productList);
+  const [searchVal, setSearchVal] = useState("");
+
+  function handleSearchClick() {
+    if (searchVal === "") {
+      setProducts(productList);
+      return;
+    }
+    const filterBySearch = productList.filter((item) => {
+      if (item.toLowerCase().includes(searchVal.toLowerCase())) {
+        return item;
+      }
+
+      return null;
+    });
+    setProducts(filterBySearch);
+  }
+
+  const containerStyles = {
+    display: "flex",
+    flexDirection: "column",
+    height: "100vh",
+    width: "100vh",
+    justifyContent: "center",
+    alignItems: "center",
+  };
+
+  const wrapperContainer = {
+    textAlign: "left",
+  };
+
+  const inputStyle = {
+    padding: "0.5rem",
+    borderRadius: "0.5rem",
+    border: "solid 2px #006700",
+    onFocus: "border-none",
+  };
+
+  const searchbarStyles = {
+    padding: "0.5rem",
+    borderRadius: "0.5rem",
+    backgroundColor: "#006700",
+    color: "#fff",
+    fontWeight: "700",
+  };
+
+  const wrapperStyles = {
+    display: "flex",
+    flexDirection: "row",
+    gap: "10px",
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={containerStyles}>
+      <div style={wrapperStyles}>
+        <input
+          onChange={(e) => setSearchVal(e.target.value)}
+          style={inputStyle}
+        />
+        <button onClick={handleSearchClick} style={searchbarStyles}>
+          Search
+        </button>
+      </div>
+      <div style={wrapperContainer}>
+        {products.map((product) => {
+          return <div style={wrapperContainer}>{product}</div>;
+        })}
+      </div>
     </div>
   );
 }
